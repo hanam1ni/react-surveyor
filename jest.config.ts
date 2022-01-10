5402cc09da99a242b2a13176dccea15caaea4b17
Miicky Jittjana <immicky.a@gmail.com>
2022-01-10 11:23:01 +0700

    Update path alias

--- jest.config.ts
+++ jest.config.ts
@@ -82,8 +82,8 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '\\.(css|less|sass|scss)$': '<rootDir>/config/__mocks__/stylesMock.ts',
+    "^@pages/(.*)$": "<rootDir>/pages/$1",
+    "\\.(css|less|sass|scss)$": "<rootDir>/config/__mocks__/stylesMock.ts",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
@@ -131,9 +131,7 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    '<rootDir>/jest.setup.ts'
-  ],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
