02cf907fe22dd07c837aa76bf489caad0e7a2b4f
Miicky Jittjana <immicky.a@gmail.com>
2022-03-18 13:15:04 +0700

    Update index content

--- cypress/integration/Home/index.spec.ts
+++ cypress/integration/Home/index.spec.ts
@@ -2,6 +2,6 @@ describe('Home page', () => {
   it('displays header', () => {
     cy.visit('/');
 
-    cy.findByText('Coming soon, Stay tune for more content').should('exist');
+    cy.findByText('Coming soon. Stay tune for more content 😎').should('exist');
   });
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <h1 className="text-white text-2xl font-bold" role="heading">
-      Coming soon, Stay tune for more content
+      Coming soon. Stay tune for more content 😎
     </h1>
   );
 };
