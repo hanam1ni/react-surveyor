792a684b2ee2a08b791e775e529adb481f4ccd6a
Miicky Jittjana <immicky.a@gmail.com>
2022-03-23 16:41:34 +0700

    Update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,css}',
+    './src/components/**/*.{js,ts,jsx,tsx,css}',
   ],
   theme: {
     extend: {
