50f21146675debc9eef322b552ca0a032f81924c
Miicky Jittjana <immicky.a@gmail.com>
2022-04-05 16:16:02 +0700

    Refresh access token when it expired

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,4 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { refreshToken } from 'services/user';
+import { setUserToken } from 'utils/userToken';
 
 const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
@@ -6,9 +8,41 @@ const httpClient = axios.create({
 
 httpClient.interceptors.response.use(
   (response) => response.data,
-  (error) =>
-    Promise.reject({ status: error.response.status, body: error.response.data })
+  (error: AxiosError) => {
+    const [{ code: errorCode }] = error.response?.data.errors;
+
+    if (errorCode == 'invalid_token') {
+      return handleInvalidAccessToken(error);
+    } else {
+      return Promise.reject({
+        status: error.response?.status,
+        body: error.response?.data,
+      });
+    }
+  }
 );
+
+const handleInvalidAccessToken = async (error: AxiosError) => {
+  try {
+    const { accessToken, refreshToken: userRefreshToken } =
+      await refreshToken();
+
+    const originalRequestConfig = error.config;
+
+    if (originalRequestConfig.headers) {
+      originalRequestConfig.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    setUserToken(accessToken, userRefreshToken);
+
+    return axios(originalRequestConfig);
+  } catch {
+    return Promise.reject({
+      status: error.response?.status,
+      body: error.response?.data,
+    });
+  }
+};
 
 export const get = async (url: string, options = {}) =>
   httpClient.get(url, options);
