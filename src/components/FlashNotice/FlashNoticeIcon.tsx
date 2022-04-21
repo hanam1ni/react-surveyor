6fd0cdfadcce14ceea310c6860469cee70ee7e87
Miicky Jittjana <immicky.a@gmail.com>
2022-04-21 11:21:49 +0700

    [#5] Refactor flash notice component

--- src/components/FlashNotice/FlashNoticeIcon.tsx
+++ src/components/FlashNotice/FlashNoticeIcon.tsx
@@ -2,23 +2,33 @@ import Image from 'next/image';
 
 import { FlashType } from '.';
 
-const FlashNoticeIcon = ({ type }: { type: FlashType }) => {
-  switch (type) {
-    case 'success':
-      return (
-        <Image src="/icon/bell.svg" width={24} height={24} alt="success icon" />
-      );
+interface IconOption {
+  iconAlt: string;
+  iconPath: string;
+}
 
-    case 'warning':
-      return (
-        <Image
-          src="/icon/warning.svg"
-          width={24}
-          height={24}
-          alt="warning icon"
-        />
-      );
-  }
+const iconOptions: Record<FlashType, IconOption> = {
+  success: {
+    iconAlt: 'success icon',
+    iconPath: '/icon/bell.svg',
+  },
+  warning: {
+    iconAlt: 'warning icon',
+    iconPath: '/icon/warning.svg',
+  },
+};
+
+const FlashNoticeIcon = ({ type }: { type: FlashType }) => {
+  const options = iconOptions[type];
+
+  return (
+    <Image
+      width={24}
+      height={24}
+      alt={options.iconAlt}
+      src={options.iconPath}
+    />
+  );
 };
 
 export default FlashNoticeIcon;
