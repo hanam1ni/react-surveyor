0222d71f0ef13aee695d84fdb652252ae4375fe5
Micky Jittjana <immicky.a@gmail.com>
2022-07-08 18:47:12 +0700

    [#13][UI] As a user, I can response to single-select survey type (#82)
    * [#13] Add choice question

---
+++ src/components/SurveyAnswer/ChoiceAnswer.tsx
@@ -0,0 +1,43 @@
+import SimpleBar from 'simplebar-react';
+
+import { SurveyAnswerProps } from '.';
+
+import 'simplebar-react/dist/simplebar.min.css';
+
+const ChoiceAnswer = ({
+  question,
+  currentResponse,
+  setResponse,
+}: SurveyAnswerProps) => {
+  const onAnswerSelect = (answerId: string) => {
+    setResponse({ questionId: question.id, answers: [{ id: answerId }] });
+  };
+
+  const answerStateClass = (answerId: string) => {
+    const isActiveAnswer = currentResponse?.answers[0].id === answerId;
+
+    if (isActiveAnswer) {
+      return 'border-y border-gray-300 text-white font-bold';
+    }
+
+    return 'text-gray-400';
+  };
+
+  return (
+    <SimpleBar className="max-h-60 pr-4" autoHide={false}>
+      {question.answers.map((answer, index) => (
+        <div
+          key={index}
+          className={`py-4 text-center cursor-pointer hover:text-white hover:font-semibold ${answerStateClass(
+            answer.id
+          )}`}
+          onClick={() => onAnswerSelect(answer.id)}
+        >
+          {answer.text}
+        </div>
+      ))}
+    </SimpleBar>
+  );
+};
+
+export default ChoiceAnswer;
--- src/components/SurveyAnswer/index.test.tsx
+++ src/components/SurveyAnswer/index.test.tsx
@@ -6,8 +6,101 @@ import { SurveyQuestion as SurveyQuestionInterface } from 'services/surveyInterf
 import { build } from '@support/factory';
 
 describe('SurveyAnswer', () => {
+  describe('given choice display type', () => {
+    const ACTIVE_ANSWER_TEXT_COLOR = 'text-white';
+    const INACTIVE_ANSWER_TEXT_COLOR = 'text-gray-400';
+
+    it('renders the answers', () => {
+      const answers = [
+        build('surveyAnswer', { text: 'First Question' }),
+        build('surveyAnswer', { text: 'Second Question' }),
+      ];
+      const question = build('surveyQuestion', {
+        displayType: 'choice',
+        answers: answers,
+      }) as SurveyQuestionInterface;
+
+      const { getByText } = render(
+        <SurveyAnswer
+          question={question}
+          currentResponse={null}
+          setResponse={jest.fn()}
+        />
+      );
+
+      expect(getByText(/First Question/)).toHaveClass(
+        INACTIVE_ANSWER_TEXT_COLOR
+      );
+      expect(getByText(/Second Question/)).toHaveClass(
+        INACTIVE_ANSWER_TEXT_COLOR
+      );
+    });
+
+    describe('click on answer', () => {
+      it('sets survey response', () => {
+        const answer = build('surveyAnswer', { text: 'First Question' });
+        const otherAnswers = [
+          build('surveyAnswer', { text: 'Second Question' }),
+          build('surveyAnswer', { text: 'Third Question' }),
+        ];
+        const question = build('surveyQuestion', {
+          displayType: 'choice',
+          answers: [answer, ...otherAnswers],
+        }) as SurveyQuestionInterface;
+        const setResponse = jest.fn();
+
+        const { getByText } = render(
+          <SurveyAnswer
+            question={question}
+            currentResponse={null}
+            setResponse={setResponse}
+          />
+        );
+
+        fireEvent.click(getByText(/First Question/));
+
+        expect(setResponse).toHaveBeenCalledWith({
+          questionId: question.id,
+          answers: [{ id: answer.id }],
+        });
+      });
+    });
+
+    describe('given valid response', () => {
+      it('sets active answer with the current response', () => {
+        const answer1 = build('surveyAnswer', { text: 'First Question' });
+        const answer2 = build('surveyAnswer', { text: 'Second Question' });
+        const question = build('surveyQuestion', {
+          displayType: 'choice',
+          answers: [answer1, answer2],
+        }) as SurveyQuestionInterface;
+
+        const { getByText } = render(
+          <SurveyAnswer
+            question={question}
+            currentResponse={{
+              questionId: question.id,
+              answers: [{ id: answer1.id }],
+            }}
+            setResponse={jest.fn()}
+          />
+        );
+
+        expect(getByText(/First Question/)).toHaveClass(
+          ACTIVE_ANSWER_TEXT_COLOR
+        );
+        expect(getByText(/Second Question/)).toHaveClass(
+          INACTIVE_ANSWER_TEXT_COLOR
+        );
+      });
+    });
+  });
+
   describe('given rating display type', () => {
-    it('renders the answers', async () => {
+    const ACTIVE_ANSWER_OPACITY = 'opacity-100';
+    const INACTIVE_ANSWER_OPACITY = 'opacity-50';
+
+    it('renders the answers', () => {
       const answers = [build('surveyAnswer'), build('surveyAnswer')];
       const question = build('surveyQuestion', {
         displayType: 'rating',
@@ -27,7 +120,7 @@ describe('SurveyAnswer', () => {
     });
 
     describe('click on answer', () => {
-      it('sets survey response', async () => {
+      it('sets survey response', () => {
         const answer = build('surveyAnswer');
         const otherAnswers = [build('surveyAnswer'), build('surveyAnswer')];
         const question = build('surveyQuestion', {
@@ -51,6 +144,37 @@ describe('SurveyAnswer', () => {
           questionId: question.id,
           answers: [{ id: answer.id }],
         });
+      });
+    });
+
+    describe('given valid response', () => {
+      it('sets active answer with the current response', () => {
+        const answer1 = build('surveyAnswer', { displayOrder: 0 });
+        const answer2 = build('surveyAnswer', { displayOrder: 1 });
+        const question = build('surveyQuestion', {
+          displayType: 'rating',
+          ratingType: 'star',
+          answers: [answer1, answer2],
+        }) as SurveyQuestionInterface;
+
+        const { getAllByAltText } = render(
+          <SurveyAnswer
+            question={question}
+            currentResponse={{
+              questionId: question.id,
+              answers: [{ id: answer1.id }],
+            }}
+            setResponse={jest.fn()}
+          />
+        );
+
+        const answerItems = getAllByAltText('star icon');
+        expect(answerItems[0].closest('div')).toHaveClass(
+          ACTIVE_ANSWER_OPACITY
+        );
+        expect(answerItems[1].closest('div')).toHaveClass(
+          INACTIVE_ANSWER_OPACITY
+        );
       });
     });
   });
