3f70fdfdb71f6a4c1ffac55c3cc083f48a7cc65c
Micky Jittjana <immicky.a@gmail.com>
2022-07-12 11:46:30 +0700

    [#17] Fix warnings

--- src/components/SurveyAnswer/TextfieldAnswer.tsx
+++ src/components/SurveyAnswer/TextfieldAnswer.tsx
@@ -32,21 +32,12 @@ const TextfieldAnswer = ({
 
   return (
     <div>
-<<<<<<< HEAD
       {question.answers.map((answer) => (
         <div key={answer.id} className="pl-2 pr-8 mb-4">
           <h2 className="mb-2 text-gray-400">{answer.text}</h2>
           <input
             className="w-full px-4 py-2 rounded-lg bg-gray-300 bg-opacity-30 backdrop-blur-3xl text-white text-lg"
             value={answerValue(answer.id)}
-=======
-      {question.answers.map((answer, index) => (
-        <div key={index} className="pl-2 pr-8 mb-4">
-          <h2 className="mb-2 text-gray-400">{answer.text}</h2>
-          <input
-            className="w-full px-4 py-2 rounded-lg bg-gray-300 bg-opacity-30 backdrop-blur-3xl text-white text-lg"
-            value={setAnswerValue(answer.id)}
->>>>>>> fe0355c ([#17] Add textfield survey type)
             onChange={(event) => handleAnswerChange(event, answer.id)}
           />
         </div>
