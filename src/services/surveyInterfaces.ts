29abfe0d3194cb9915711dc25a6e05ad10d2246d
Micky Jittjana <immicky.a@gmail.com>
2022-07-27 10:21:23 +0700

    [#14][UI] As a user, I can response to multi-select survey type (#97)
    * [#14] Add multi select survey answer

--- src/services/surveyInterfaces.ts
+++ src/services/surveyInterfaces.ts
@@ -19,7 +19,7 @@ interface SurveyQuestion {
   displayType: NonQuestionType | QuestionType;
   ratingType?: RatingType;
   text: string;
-  pick: string;
+  pick: PickType;
   isMandatory: boolean;
   answers: SurveyAnswer[];
 }
@@ -44,6 +44,11 @@ enum RatingType {
   SMILEY = 'smiley',
 }
 
+enum PickType {
+  one = 'one',
+  any = 'any',
+}
+
 interface SurveyAnswer {
   id: string;
   displayOrder: number;
@@ -56,7 +61,7 @@ interface SurveyResponse {
   answers: { id: string; answer?: string }[];
 }
 
-export { NonQuestionType, QuestionType, RatingType };
+export { NonQuestionType, PickType, QuestionType, RatingType };
 
 export type {
   Survey,
